901ae3bef8d2bb55430b50a162385f623165bfef
DeepDuvedi <104484027+DeepDuvedi@users.noreply.github.com>
2023-09-24 15:18:41 +0530

    Add files via upload

---
+++ APIDemo/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+//using System.Data.sql;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students =new List<Student>();
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {   
+            return students.FirstOrDefault(s=>s.ID==id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student value)
+        {
+            students.Add(value);
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student value)
+        {
+            int i= students.FindIndex(s => s.ID == id);    
+            if(i>=0)
+                students[i]=value;
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            students.RemoveAll(s=>s.ID==id);
+        }
+
+       
+    }
+}
---
+++ APIDemo/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace APIDemo
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
